3a1413dc7b0e68097fcf96a446120085d98e958f
Wu Mianzhi <1427207878@qq.com>
2025-02-10 18:08:25 +0800

    fix api

--- src/api_selection.py
+++ src/api_selection.py
@@ -22,21 +22,21 @@ class APIDocAgent:
         # self.llm = ChatOpenAI(
         #     model="deepseek-ai/DeepSeek-V2.5",
         #     temperature=0,
-        #     openai_api_key="sk-lutarydeakkcpstmsokjfiaqycdgaogkqjuomndbwwjeqyaj",
+        #     openai_api_key="",
         #     base_url="https://api.siliconflow.cn/v1"
 
         # )
         # self.llm = ChatOpenAI(
         #     # model="deepseek-ai/DeepSeek-V2.5",
         #     temperature=0,
-        #     openai_api_key="sk-MCCLTj4o2BLGJc0yX9CKqEAQ6zTWhIxfTVG9cd67v6xZdjtf",
+        #     openai_api_key="",
         #     base_url="https://xiaoai.plus/v1"
 
         # )
         self.llm = ChatOpenAI(
             model = "xdeepseekv3",
             temperature=0.5,
-            openai_api_key="sk-WQgfUw9WdnuWRDC1521657C94676420b97D5084c89698647",
+            openai_api_key="",
             base_url="https://maas-api.cn-huabei-1.xf-yun.com/v1"
 
         )
